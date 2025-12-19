30cc62805b7e59e7cdaba2780fdaa431c5ca29c3
Benjilo12 <benjamindarteyy@gmail.com>
2025-12-19 13:58:52 +0000

    Fix deployment workflow

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 function page() {
   return (
-    <div className="text-3xl text-yellow-400  mx-auto max-w-7xl justify-items-center items-center">
+    <div className="text-3xl text-green-400  mx-auto max-w-7xl justify-items-center items-center">
       <h1>hello this is my website</h1>
     </div>
   );
