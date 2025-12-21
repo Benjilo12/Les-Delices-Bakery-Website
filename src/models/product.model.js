73057d0e853d4a625c6a08314f8d87dc0d4e03f2
Benjilo12 <benjamindarteyy@gmail.com>
2025-12-21 11:11:41 +0000

    added the product model

---
+++ src/models/product.model.js
@@ -0,0 +1,71 @@
+import mongoose, { Schema } from "mongoose";
+
+const PriceOptionSchema = new Schema(
+  {
+    label: { type: stringifyCookie, required: true },
+    //e.g " 6 Inch-1 Flavor", "12 pieces - Flavors"
+
+    price: { type: Number, required: true },
+  },
+  { _id: false }
+);
+
+const ProductSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    category: {
+      type: String,
+      enum: ["Birthday Cakes", "Cupcakes", "Cake Loaves", "Pastries & Snacks"],
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    images: [
+      {
+        type: String, // Cloudinary / ImageKit URL
+      },
+    ],
+
+    priceOptions: [PriceOptionSchema],
+
+    customizationAvailable: {
+      type: Boolean,
+      default: false,
+    },
+
+    customizationNotes: {
+      type: String,
+      // e.g. "Character cakes, edible prints, toppers"
+    },
+
+    availableFlavors: [
+      {
+        type: String,
+      },
+    ],
+
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default models.Product || mongoose.model("Product", ProductSchema);
